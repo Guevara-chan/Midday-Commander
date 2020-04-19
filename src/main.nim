1aa11c09eadf287e30af6d99f8a15d21a4d1c178
Victoria Guevara <Guevara-chan@mail.ru>
2020-04-19 09:38:46 +0300

    Add files via upload

--- src/main.nim
+++ src/main.nim
@@ -25,6 +25,7 @@ when not defined(Meta):
     template abort(reason = "")     = raise newException(abort_ex, reason)
     template control_down(): bool   = KEY_Left_Control.IsKeyDown()  or KEY_Right_Control.IsKeyDown()
     template shift_down(): bool     = KEY_Left_Shift.IsKeyDown()    or KEY_Right_Shift.IsKeyDown()
+    template alt_down(): bool       = KEY_Left_Alt.IsKeyDown()      or KEY_Right_Alt.IsKeyDown()
     template undot(ext: string): string                            = ext.runeSubstr((" " & ext).searchExtPos)
     template fit(txt: string, size: int, filler = ' '): string     = txt.align(size, filler.Rune).runeSubStr 0, size
     template fit_left(txt: string, size: int, filler = ' '): string= txt.alignLeft(size, filler.Rune).runeSubStr 0,size
